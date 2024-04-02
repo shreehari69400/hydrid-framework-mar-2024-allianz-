018348bc6b3e02cb125a4900910309389a2ca310
Shree Hari <shreehari69400@gmail.com>
2024-04-02 00:26:28 -0700

    first two method in Maven Project

---
+++ src/test/java/com/allianz/test/LoginUITest.java
@@ -0,0 +1,17 @@
+package com.allianz.test;
+
+import org.testng.annotations.Test;
+
+public class LoginUITest {
+
+	@Test
+	public void titleTest() {
+		System.out.println("test");
+	}
+	
+	@Test
+	public void applicationTest() {
+		System.out.println("apptest");
+	}
+	}
+
