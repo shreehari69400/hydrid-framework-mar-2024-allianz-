ba913af748d101b81adeabcab51997db61490b02
Shree Hari <shreehari69400@gmail.com>
2024-04-03 23:46:12 -0700

    assert actual expected

--- src/test/java/com/allianz/test/LoginUITest.java
+++ src/test/java/com/allianz/test/LoginUITest.java
@@ -1,17 +1,42 @@
 package com.allianz.test;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginUITest {
 
-	@Test(invocationCount = 3)
+	@Test
 	public void titleTest() {
-		System.out.println("test");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
+		String actualTitle=driver.getTitle();
+		Assert.assertEquals(actualTitle, "OrangeHRM");
+		
 	}
 	
-	@Test (priority = 1)
-	public void applicationTest() {
-		System.out.println("apptest");
+	@Test 
+	public void applicationDescriptionTest() {
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
+		
+		String actualText=driver.findElement(By.xpath("//p[contains(normalize-space(),'OS')]")).getText();
+		Assert.assertEquals(actualText, "OrangeHRM OS 5.6.1");
+		
+		
+		
+		
+		
 	}
 	}
 
