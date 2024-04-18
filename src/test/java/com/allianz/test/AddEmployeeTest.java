4c2bd57bbb19587bc0f028b1b686458b58ac832d
Shree Hari <shreehari69400@gmail.com>
2024-04-17 23:36:22 -0700

    file upload inside the SRC

--- src/test/java/com/allianz/test/AddEmployeeTest.java
+++ src/test/java/com/allianz/test/AddEmployeeTest.java
@@ -13,30 +13,23 @@ public class AddEmployeeTest extends AutomationWrapper {
 	@Test
 	public void uploadInvalidPictureTest ()
 	{
-		/*
-		 * File file=new
-		 * File("src/test/resources/istockphoto-157373207-1024x1024.jpeg"); String
-		 * path=file.getAbsolutePath();
-		 */
 		
+		File file=new File("src/test/resources/files/xpath.txt");
+		String path=file.getAbsolutePath();
 		
 		driver.findElement(By.name("username")).sendKeys("Admin");
 		driver.findElement(By.name("password")).sendKeys("admin123");
-		driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--main orangehrm-login-button']")).click();
+		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
 		
-		driver.findElement(By.xpath("(//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name'])[2]")).click();
+		driver.findElement(By.xpath("//span[text()='PIM']")).click();
 		driver.findElement(By.linkText("Add Employee")).click();
-		//driver.findElement(By.xpath("//button[@class='oxd-icon-button oxd-icon-button--solid-main employee-image-action']")).click();
 		
-	driver.findElement(By.xpath("//input[@type='file']")).sendKeys("C:\\Users\\Administrator\\Downloads\\Balanotes.txt");
-
-	String actualError=driver.findElement(By.xpath("//span[contains(normalize-space(),'File type')]")).getText();
-	Assert.assertTrue(actualError.contains("File type not"));
+		driver.findElement(By.xpath("//input[@type='file']")).sendKeys(path);
 		
-		//click on PIMmenu
-	//click on add employee
-	//upload the file 
-	//validate the eeor 
+		String actualError=driver.findElement(By.xpath("//span[contains(normalize-space(),'File type')]")).getText();
+		Assert.assertTrue(actualError.contains("File type not allowed")); 
+		
+		
 
 }
 }
